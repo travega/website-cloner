f6f6f36c148c7507efa9be13cf86a846b16f51c3
Andrew Scott <andrewscott@andrewsc-ltm.internal.salesforce.com>
2019-01-29 19:26:42 +0000

    clone script update

---
+++ clone.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+rm -rf ./website
+mkdir ./website
+
+wget --recursive --no-clobber --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains ntoretail.com --no-parent https://$1
+
+mv $1/index.html website
+rm -rf $1
